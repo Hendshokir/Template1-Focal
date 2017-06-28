8274bb3931d2bd73467bfcccdf58d27a59683b55
Hend Shokir <hendshokir@gmail.com>
2017-06-28 13:55:39 +0200

    upload the template

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$(function(){
+	'use strict';
+	
+	//Trigger Nice Scroll
+	 $("html").niceScroll({
+		 cursorcolor:'#2ECC71',
+		 cursorborder:'1px solid #1ABC9C',
+		 cursorwidth:'10px',
+		 
+	 });
+});
+
+
+
